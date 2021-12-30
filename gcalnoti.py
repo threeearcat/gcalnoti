e5bd614b11d02b40065ed690cfd70f593db0f6d7
Dae R. Jeong <threeearcat@gmail.com>
2021-12-30 21:28:36 +0900

    Initial commit
    Ref)
    https://github.com/googleapis/google-api-python-client/blob/main/samples/calendar_api/calendar_sample.py

---
+++ gcalnoti.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+
+def main():
+    """Shows basic usage of the People API.
+    Prints the name of the first 10 connections.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                './client_secret.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build('calendar', 'v3', credentials=creds)
+
+        page_token = None
+        while True:
+            calendar_list = service.calendarList().list(
+                pageToken=page_token).execute()
+            for calendar_list_entry in calendar_list['items']:
+                print(calendar_list_entry['summary'])
+                page_token = calendar_list.get('nextPageToken')
+            if not page_token:
+                break
+    except HttpError as err:
+        print(err)
+
+
+if __name__ == '__main__':
+    main()
